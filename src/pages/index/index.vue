120bf6584ce97ea33241797d25ce6a7cfdd95926
hzj <1471426152@qq.com>
2021-04-26 14:39:33 +0800

    模拟数据展示

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
   <view class="content">
     <view class="header">
       <u-navbar
-      height='50'
+        height="50"
         title-color="white"
         :is-back="false"
         :background="background"
@@ -157,67 +157,77 @@ export default class index extends Vue {
       price: 35,
       title: "北国风光，千里冰封，万里雪飘",
       shop: "李白杜甫白居易旗舰店",
-      image: "http://pic.sc.chinaz.com/Files/pic/pic9/202002/zzpic23327_s.jpg",
+      image: "https://cdn.uviewui.com/uview/swiper/1.jpg",
     },
     {
       price: 75,
       title: "望长城内外，惟余莽莽",
       shop: "李白杜甫白居易旗舰店",
-      image: "http://pic.sc.chinaz.com/Files/pic/pic9/202002/zzpic23325_s.jpg",
+      image:
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3158103848,3930143469&fm=26&gp=0.jpg",
     },
     {
       price: 385,
       title: "大河上下，顿失滔滔",
       shop: "李白杜甫白居易旗舰店",
-      image: "http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg",
+      image:
+        "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1123325709,3889625062&fm=26&gp=0.jpg",
     },
     {
       price: 784,
       title: "欲与天公试比高",
       shop: "李白杜甫白居易旗舰店",
-      image: "http://pic2.sc.chinaz.com/Files/pic/pic9/202002/zzpic23369_s.jpg",
+      image:
+        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3610741644,1365751844&fm=26&gp=0.jpg",
     },
     {
       price: 7891,
       title: "须晴日，看红装素裹，分外妖娆",
       shop: "李白杜甫白居易旗舰店",
-      image: "http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2130_s.jpg",
+      image:
+        "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3769855956,3407723647&fm=26&gp=0.jpg",
     },
     {
       price: 2341,
       shop: "李白杜甫白居易旗舰店",
       title: "江山如此多娇，引无数英雄竞折腰",
-      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23346_s.jpg",
+      image:
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=856416488,2118015371&fm=26&gp=0.jpg",
     },
     {
       price: 661,
       shop: "李白杜甫白居易旗舰店",
       title: "惜秦皇汉武，略输文采",
-      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23344_s.jpg",
+      image:
+        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1889660434,1069288854&fm=26&gp=0.jpg",
     },
     {
       price: 1654,
       title: "唐宗宋祖，稍逊风骚",
       shop: "李白杜甫白居易旗舰店",
-      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg",
+      image:
+        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2410275967,581601604&fm=26&gp=0.jpg",
     },
     {
       price: 1678,
       title: "一代天骄，成吉思汗",
       shop: "李白杜甫白居易旗舰店",
-      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg",
+      image:
+        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=353433341,2564518283&fm=26&gp=0.jpg",
     },
     {
       price: 924,
       title: "只识弯弓射大雕",
       shop: "李白杜甫白居易旗舰店",
-      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg",
+      image:
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1196781438,2822532451&fm=26&gp=0.jpg",
     },
     {
       price: 8243,
       title: "俱往矣，数风流人物，还看今朝",
       shop: "李白杜甫白居易旗舰店",
-      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg",
+      image:
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=405276296,856841904&fm=26&gp=0.jpg",
     },
   ];
   background: any = {
