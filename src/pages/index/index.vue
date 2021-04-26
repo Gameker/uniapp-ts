2a4969e78043af4b4c412038b5e07e052e351b01
hzj <1471426152@qq.com>
2021-04-26 14:25:26 +0800

    初始化首页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,104 @@
 <template>
   <view class="content">
-    <view>
-      <text class="title">{{ title }}</text>
+    <view class="header">
+      <u-navbar
+      height='50'
+        title-color="white"
+        :is-back="false"
+        :background="background"
+        title="剑未配妥，出门已是江湖"
+      ></u-navbar>
+    </view>
+    <view class="main u-skeleton">
+      <view style="padding: 25px 25px 0 25px">
+        <u-swiper
+          :list="list2"
+          indicator-pos="topRight"
+          :title="true"
+          class="u-skeleton-rect"
+        ></u-swiper>
+      </view>
+      <view class="wrap">
+        <!-- <u-button @click="clear">清空列表</u-button> -->
+        <u-waterfall v-model="flowList" ref="uWaterfall">
+          <template v-slot:left="{ leftList }">
+            <view
+              class="demo-warter"
+              v-for="(item, index) in leftList"
+              :key="index"
+            >
+              <!-- 警告：微信小程序中需要hx2.8.11版本才支持在template中结合其他组件，比如下方的lazy-load组件 -->
+              <u-lazy-load
+                threshold="-450"
+                border-radius="10"
+                :image="item.image"
+                :index="index"
+              ></u-lazy-load>
+              <view class="demo-title">
+                {{ item.title }}
+              </view>
+              <view class="demo-price"> {{ item.price }}元 </view>
+              <view class="demo-tag">
+                <view class="demo-tag-owner"> 自营 </view>
+                <view class="demo-tag-text"> 放心购 </view>
+              </view>
+              <view class="demo-shop">
+                {{ item.shop }}
+              </view>
+              <u-icon
+                name="close-circle-fill"
+                color="#fa3534"
+                size="34"
+                class="u-close"
+                @click="remove(item.id)"
+              ></u-icon>
+            </view>
+          </template>
+          <template v-slot:right="{ rightList }">
+            <view
+              class="demo-warter"
+              v-for="(item, index) in rightList"
+              :key="index"
+            >
+              <u-lazy-load
+                threshold="-450"
+                border-radius="10"
+                :image="item.image"
+                :index="index"
+              ></u-lazy-load>
+              <view class="demo-title">
+                {{ item.title }}
+              </view>
+              <view class="demo-price"> {{ item.price }}元 </view>
+              <view class="demo-tag">
+                <view class="demo-tag-owner"> 自营 </view>
+                <view class="demo-tag-text"> 放心购 </view>
+              </view>
+              <view class="demo-shop">
+                {{ item.shop }}
+              </view>
+              <u-icon
+                name="close-circle-fill"
+                color="#fa3534"
+                size="34"
+                class="u-close"
+                @click="remove(item.id)"
+              ></u-icon>
+            </view>
+          </template>
+        </u-waterfall>
+        <u-loadmore
+          bg-color="rgb(240, 240, 240)"
+          :status="loadStatus"
+          @loadmore="addRandomData"
+        ></u-loadmore>
+      </view>
+      <!--引用组件 骨架屏-->
+      <u-skeleton
+        :loading="false"
+        :animation="true"
+        bgColor="#FFF"
+      ></u-skeleton>
     </view>
   </view>
 </template>
@@ -10,31 +107,207 @@
 import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class index extends Vue {
-  title: string = "Hello";
+  $u: any;
+  onLoad() {
+    this.addRandomData();
+  }
+  onReachBottom() {
+    this.loadStatus = "loading";
+    // 模拟数据加载
+    setTimeout(() => {
+      this.addRandomData();
+      this.loadStatus = "loadmore";
+    }, 1000);
+  }
+  addRandomData() {
+    for (let i = 0; i < 10; i++) {
+      let index = this.$u.random(0, this.list.length - 1);
+      // 先转成字符串再转成对象，避免数组对象引用导致数据混乱
+      let item = JSON.parse(JSON.stringify(this.list[index]));
+      item.id = this.$u.guid();
+      this.flowList.push(item);
+    }
+  }
+  remove(id: number) {
+    (this.$refs.uWaterfall as any).remove(id);
+  }
+  // clear() {
+  //   this.$refs.uWaterfall.clear();
+  // }
+  keyword: string = "遥看瀑布挂前川";
+
+  list2 = [
+    {
+      image: "https://cdn.uviewui.com/uview/swiper/1.jpg",
+      title: "昨夜星辰昨夜风，画楼西畔桂堂东",
+    },
+    {
+      image: "https://cdn.uviewui.com/uview/swiper/2.jpg",
+      title: "身无彩凤双飞翼，心有灵犀一点通",
+    },
+    {
+      image: "https://cdn.uviewui.com/uview/swiper/3.jpg",
+      title: "谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳",
+    },
+  ];
+  loadStatus = "loadmore";
+  flowList: any = [];
+  list = [
+    {
+      price: 35,
+      title: "北国风光，千里冰封，万里雪飘",
+      shop: "李白杜甫白居易旗舰店",
+      image: "http://pic.sc.chinaz.com/Files/pic/pic9/202002/zzpic23327_s.jpg",
+    },
+    {
+      price: 75,
+      title: "望长城内外，惟余莽莽",
+      shop: "李白杜甫白居易旗舰店",
+      image: "http://pic.sc.chinaz.com/Files/pic/pic9/202002/zzpic23325_s.jpg",
+    },
+    {
+      price: 385,
+      title: "大河上下，顿失滔滔",
+      shop: "李白杜甫白居易旗舰店",
+      image: "http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg",
+    },
+    {
+      price: 784,
+      title: "欲与天公试比高",
+      shop: "李白杜甫白居易旗舰店",
+      image: "http://pic2.sc.chinaz.com/Files/pic/pic9/202002/zzpic23369_s.jpg",
+    },
+    {
+      price: 7891,
+      title: "须晴日，看红装素裹，分外妖娆",
+      shop: "李白杜甫白居易旗舰店",
+      image: "http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2130_s.jpg",
+    },
+    {
+      price: 2341,
+      shop: "李白杜甫白居易旗舰店",
+      title: "江山如此多娇，引无数英雄竞折腰",
+      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23346_s.jpg",
+    },
+    {
+      price: 661,
+      shop: "李白杜甫白居易旗舰店",
+      title: "惜秦皇汉武，略输文采",
+      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23344_s.jpg",
+    },
+    {
+      price: 1654,
+      title: "唐宗宋祖，稍逊风骚",
+      shop: "李白杜甫白居易旗舰店",
+      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg",
+    },
+    {
+      price: 1678,
+      title: "一代天骄，成吉思汗",
+      shop: "李白杜甫白居易旗舰店",
+      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg",
+    },
+    {
+      price: 924,
+      title: "只识弯弓射大雕",
+      shop: "李白杜甫白居易旗舰店",
+      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg",
+    },
+    {
+      price: 8243,
+      title: "俱往矣，数风流人物，还看今朝",
+      shop: "李白杜甫白居易旗舰店",
+      image: "http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg",
+    },
+  ];
+  background: any = {
+    // backgroundColor: "#001f3f",
+    // 导航栏背景图
+    // background: "url(https://cdn.uviewui.com/uview/swiper/1.jpg) no-repeat",
+    // // 还可以设置背景图size属性
+    // backgroundSize: "cover",
+
+    // 渐变色
+    backgroundImage: "linear-gradient(-90deg,#a050d7 0,#f7889c 100%)",
+  };
 }
 </script>
 
 <style>
-.content {
+/* page不能写带scope的style标签中，否则无效 */
+page {
+  background-color: rgb(240, 240, 240);
+}
+</style>
+<style lang="scss" scoped>
+.wrap {
+  padding: 40rpx;
+}
+
+.demo-warter {
+  border-radius: 8px;
+  margin: 5px;
+  background-color: #ffffff;
+  padding: 8px;
+  position: relative;
+}
+
+.u-close {
+  position: absolute;
+  top: 32rpx;
+  right: 32rpx;
+}
+
+.demo-image {
+  width: 100%;
+  border-radius: 4px;
+}
+
+.demo-title {
+  font-size: 30rpx;
+  margin-top: 5px;
+  color: $u-main-color;
+}
+
+.demo-tag {
   display: flex;
-  flex-direction: column;
+  margin-top: 5px;
+}
+
+.demo-tag-owner {
+  background-color: $u-type-error;
+  color: #ffffff;
+  display: flex;
   align-items: center;
-  justify-content: center;
+  padding: 4rpx 14rpx;
+  border-radius: 50rpx;
+  font-size: 20rpx;
+  line-height: 1;
 }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin: 200rpx auto 50rpx auto;
-}
-
-.text-area {
+.demo-tag-text {
+  border: 1px solid $u-type-primary;
+  color: $u-type-primary;
+  margin-left: 10px;
+  border-radius: 50rpx;
+  line-height: 1;
+  padding: 4rpx 14rpx;
   display: flex;
-  justify-content: center;
+  align-items: center;
+  border-radius: 50rpx;
+  font-size: 20rpx;
 }
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+.demo-price {
+  font-size: 30rpx;
+  color: $u-type-error;
+  margin-top: 5px;
 }
+
+.demo-shop {
+  font-size: 22rpx;
+  color: $u-tips-color;
+  margin-top: 5px;
+}
+</style>
 </style>
