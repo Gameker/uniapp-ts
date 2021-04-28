938fde05f130b520e1f7ee986ea0a27dccd397c3
hzj <1471426152@qq.com>
2021-04-28 15:34:42 +0800

    modify

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,8 @@ import '@/util/ajax'
 import uView from "uview-ui";
 Vue.use(uView);
 
-import { myRequest } from './util/http'
-Vue.prototype.$http = myRequest
-
 // 让 vue 识别全局方法/变量
 declare module 'vue/types/vue' {
-    interface Vue {
-        $http: typeof myRequest;
-    }
     interface Vue {
         $u: typeof uView;
     }
