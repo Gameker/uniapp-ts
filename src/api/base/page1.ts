b34159318c7aa36405ce0684ed590f2572bf7985
hzj <1471426152@qq.com>
2021-04-28 15:14:45 +0800

    modify

--- src/api/base/page1.ts
+++
@@ -1,8 +0,0 @@
-import { myRequest } from '../util/http'
-
-
-
-export const getSwiper = async (data: any, method: string, url?: string) => {
-    const res = await myRequest({ url, data, method });
-    console.log(res, 222);
-}
