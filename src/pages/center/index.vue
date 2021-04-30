b512424fa3e3dfe6310b743bf5e118199dd11f4b
hzj <1471426152@qq.com>
2021-04-30 15:04:04 +0800

    个人中心

--- src/pages/center/index.vue
+++ src/pages/center/index.vue
@@ -5,7 +5,55 @@
       <Header :options="options"></Header>
     </view>
     <!-- 内容 -->
-    <view class="main"></view>
+    <view class="main">
+      <view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30">
+        <view class="u-m-r-10">
+          <u-avatar
+            :src="pic"
+            size="140"
+            mode="square"
+            style="margin-right: 20rpx"
+          ></u-avatar>
+        </view>
+        <view class="u-flex-1">
+          <view class="u-font-18 u-p-b-5">NiuA</view>
+          <view class="u-font-14 u-tips-color">
+            <span style="margin-right: 16rpx">昵称:</span>巷子达人</view
+          >
+          <view class="u-font-14 u-tips-color">
+            <span style="margin-right: 16rpx">微信号:</span> x695414235</view
+          >
+        </view>
+
+        <view class="u-m-l-10 u-p-10">
+          <u-icon name="scan" color="#969799" size="28"></u-icon>
+        </view>
+        <view class="u-m-l-10 u-p-10">
+          <u-icon name="arrow-right" color="#969799" size="28"></u-icon>
+        </view>
+      </view>
+
+      <view class="u-m-t-20">
+        <u-cell-group>
+          <u-cell-item icon="rmb-circle" title="支付"></u-cell-item>
+        </u-cell-group>
+      </view>
+
+      <view class="u-m-t-20">
+        <u-cell-group>
+          <u-cell-item icon="star" title="收藏"></u-cell-item>
+          <u-cell-item icon="photo" title="相册"></u-cell-item>
+          <u-cell-item icon="coupon" title="卡券"></u-cell-item>
+          <u-cell-item icon="heart" title="关注"></u-cell-item>
+        </u-cell-group>
+      </view>
+
+      <view class="u-m-t-20">
+        <u-cell-group>
+          <u-cell-item icon="setting" title="设置"></u-cell-item>
+        </u-cell-group>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -16,9 +64,12 @@ import Header from "@/components/header.vue";
 export default class index extends Vue {
   //头部组件参数，还有：height，color，background:{}等可选
   options: any = {
-    title: "个人中心",
+    background: {
+      backgroundColor: "white",
+    },
   };
-
+  pic: string = "/static/1.jpg";
+  show: boolean = true;
   async getSwiper() {
     let params = {
       KeyboardEvent: "1111",
@@ -34,5 +85,12 @@ export default class index extends Vue {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+page {
+  background-color: #ededed;
+}
+
+.user-box {
+  background-color: #fff;
+}
 </style>
